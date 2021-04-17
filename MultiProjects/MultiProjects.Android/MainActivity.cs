ff5f100323720a43d20fdb68a65536956cdc99d7
Shina <Shina@DESKTOP-LSDO2U3>
2021-04-17 22:35:58 +0100

    tested push notification

--- MultiProjects/MultiProjects.Android/MainActivity.cs
+++ MultiProjects/MultiProjects.Android/MainActivity.cs
@@ -6,6 +6,9 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using System.Threading.Tasks;
+using Firebase.Iid;
+using Firebase;
 
 namespace MultiProjects.Droid
 {
@@ -20,7 +23,54 @@ namespace MultiProjects.Droid
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             global::Xamarin.Forms.FormsMaterial.Init(this, savedInstanceState);
+           //CreateNotificationChannel();
+            //  var token = GetToken().Result;
+            // FirebaseApp.InitializeApp(Application.Context);
+            // var t = FirebaseInstanceId.Instance.GetToken("334574478674", "FCM");
             LoadApplication(new App());
+        }
+
+        static readonly string TAG = "MainActivity";
+
+        internal static readonly string CHANNEL_ID = "my_notification_channel";
+        internal static readonly int NOTIFICATION_ID = 100;
+        void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification
+                // channel on older versions of Android.
+                return;
+            }
+
+            var channel = new NotificationChannel(CHANNEL_ID,
+                                                  "FCM Notifications",
+                                                  NotificationImportance.Default)
+            {
+
+                Description = "Firebase Cloud Messages appear in this channel"
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(Android.Content.Context.NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
+        }
+
+        private async Task<string> GetToken()  
+        {
+            string token =string.Empty;
+            await Task.Run(() =>
+            {
+                 token = FirebaseInstanceId.Instance.GetToken("334574478674", "FCM");
+                    Console.WriteLine("the token {0}", token);
+                // https://firebase.google.com/docs/reference/android/com/google/firebase/iid/FirebaseInstanceId.html#public-string-gettoken-string-senderid,-string-scope
+                if (token != "")
+                {
+                    // update saved token, send it to the backend, etc.
+                }
+                
+            });
+            return token;
         }
     }
 }
